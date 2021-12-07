da73ad07854c37f4946257bdb77e45d013ef57d3
Ben Arnold <ben@arnoldfamily.me.uk>
2021-12-07 16:00:11 +0000

    Shared library

--- src/SFA.DAS.ApprenticeAccounts.Jobs/Infrastructure/Startup.cs
+++ src/SFA.DAS.ApprenticeAccounts.Jobs/Infrastructure/Startup.cs
@@ -1,14 +1,8 @@
 ﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
-using Microsoft.Azure.ServiceBus;
-using Microsoft.Azure.ServiceBus.Core;
-using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using NServiceBus;
-using NServiceBus.ObjectBuilder;
 using SFA.DAS.ApprenticeAccounts.Jobs.Infrastructure;
-using System;
-using System.Threading.Tasks;
+using SFA.DAS.NServiceBus.AzureFunctions.ServiceBus;
 
 [assembly: FunctionsStartup(typeof(Startup))]
 [assembly: NServiceBusTriggerFunction(Startup.EndpointName)]
@@ -19,103 +13,16 @@ namespace SFA.DAS.ApprenticeAccounts.Jobs.Infrastructure
     {
         public const string EndpointName = "Bob";
 
+        public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder builder)
+        {
+            //builder.ConfigureConfiguration();
+        }
+
         public override void Configure(IFunctionsHostBuilder builder)
         {
-            //AutoSubscribeToQueues.CreateQueuesWithReflection(
-            //    builder.GetContext().Configuration)
-            //    .GetAwaiter().GetResult();
+            builder.Services.AddLogging();
 
-            //builder.Services.AddSingleton<InProcessFunctionEndpoint>
-
-            builder.UseNServiceBus(() =>
-            {
-                var configuration = new ServiceBusTriggeredEndpointConfiguration(
-                                endpointName: EndpointName,
-                                connectionStringName: "NServiceBusConnectionString");
-                configuration.AdvancedConfiguration.SendFailedMessagesTo($"{EndpointName}-error");
-                configuration.LogDiagnostics();
-                return configuration;
-            });
-
-            builder.Services.Decorate<IFunctionEndpoint>((ep, sp) =>
-            {
-                return ep;
-            });
-            //builder.Services.
-
-            //builder.Services.AddSingleton<AutoStartableEndpointWithExternallyManagedContainer>();
-            //builder.Services.Decorate<IStartableEndpointWithExternallyManagedContainer, AutoStartableEndpointWithExternallyManagedContainer>();
+            builder.UseAzureFunctionNServiceBus(EndpointName);
         }
     }
-
-    internal class AutoStartableEndpointWithExternallyManagedContainer : IStartableEndpointWithExternallyManagedContainer
-    {
-        private readonly IStartableEndpointWithExternallyManagedContainer decorated;
-
-        public AutoStartableEndpointWithExternallyManagedContainer(
-            IStartableEndpointWithExternallyManagedContainer decorated)
-            => this.decorated = decorated;
-
-        public Lazy<NServiceBus.IMessageSession> MessageSession => decorated.MessageSession;
-
-        public Task<IEndpointInstance> Start(IBuilder builder)
-        {
-            return decorated.Start(builder);
-        }
-    }
-
-    //internal class AutoCreateFunctionEndpoint : FunctionEndpoint
-    //{
-    //    private readonly IFunctionEndpoint decorated;
-
-    //    public AutoCreateFunctionEndpoint(IStartableEndpointWithExternallyManagedContainer externallyManagedContainerEndpoint, ServiceBusTriggeredEndpointConfiguration configuration, IServiceProvider serviceProvider)
-    //        : base(externallyManagedContainerEndpoint, configuration, serviceProvider)
-    //    {
-    //    }
-
-    //    //public AutoCreateFunctionEndpoint(IFunctionEndpoint decorated) => this.decorated = decorated;
-
-    //    public Task Process(Message message, ExecutionContext executionContext, IMessageReceiver messageReceiver, ILogger functionsLogger)
-    //        => decorated.Process(message, executionContext, messageReceiver, functionsLogger);
-
-    //    public Task Process(Message message, ExecutionContext executionContext, ILogger functionsLogger)
-    //        => throw new NotImplementedException();
-
-    //    public Task Publish(object message, PublishOptions options, ExecutionContext executionContext, ILogger functionsLogger)
-    //        => decorated.Publish(message, options, executionContext, functionsLogger);
-
-    //    public Task Publish<T>(Action<T> messageConstructor, PublishOptions options, ExecutionContext executionContext, ILogger functionsLogger)
-    //        => decorated.Publish(messageConstructor, options, executionContext, functionsLogger);
-
-    //    public Task Publish(object message, ExecutionContext executionContext, ILogger functionsLogger)
-    //        => decorated.Publish(message, executionContext, functionsLogger);
-
-    //    public Task Publish<T>(Action<T> messageConstructor, ExecutionContext executionContext, ILogger functionsLogger)
-    //        => decorated.Publish(messageConstructor, executionContext, functionsLogger);
-
-    //    public Task Send(object message, SendOptions options, ExecutionContext executionContext, ILogger functionsLogger)
-    //        => decorated.Send(message, options, executionContext, functionsLogger);
-
-    //    public Task Send(object message, ExecutionContext executionContext, ILogger functionsLogger)
-    //        => decorated.Send(message, executionContext, functionsLogger);
-
-    //    public Task Send<T>(Action<T> messageConstructor, SendOptions options, ExecutionContext executionContext, ILogger functionsLogger)
-    //        => decorated.Send(messageConstructor, options, executionContext, functionsLogger);
-
-    //    public Task Send<T>(Action<T> messageConstructor, ExecutionContext executionContext, ILogger functionsLogger)
-    //        => decorated.Send(messageConstructor, executionContext, functionsLogger);
-
-    //    public Task Subscribe(Type eventType, SubscribeOptions options, ExecutionContext executionContext, ILogger functionsLogger)
-    //        => decorated.Subscribe(eventType, options, executionContext, functionsLogger);
-
-    //    public Task Subscribe(Type eventType, ExecutionContext executionContext, ILogger functionsLogger)
-    //        => decorated.Subscribe(eventType, executionContext, functionsLogger);
-
-    //    public Task Unsubscribe(Type eventType, UnsubscribeOptions options, ExecutionContext executionContext, ILogger functionsLogger)
-    //        => decorated.Unsubscribe(eventType, options, executionContext, functionsLogger);
-
-    //    public Task Unsubscribe(Type eventType, ExecutionContext executionContext, ILogger functionsLogger)
-    //        => decorated.Unsubscribe(eventType, executionContext, functionsLogger);
-    //}
-
 }
---
+++ src/SFA.DAS.NServiceBus.AzureFunctions.ServiceBus/AutoSubscribeToQueues.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.ServiceBus.Management;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.NServiceBus.AzureFunctions.ServiceBus
+{
+    public static class AutoSubscribeToQueues
+    {
+        public static async Task CreateQueues(
+            Assembly assemblyWithTriggerAttribute,
+            IConfiguration configuration,
+            string connectionStringName = "AzureWebJobsServiceBus",
+            string? errorQueue = null,
+            string topicName = "bundle-1",
+            ILogger? logger = null)
+        {
+            var connectionString = configuration.GetValue<string>(connectionStringName);
+            var managementClient = new ManagementClient(connectionString);
+            await CreateQueuesWithReflection(assemblyWithTriggerAttribute, managementClient, errorQueue, topicName, logger);
+        }
+
+        private static async Task CreateQueuesWithReflection(
+            Assembly assemblyWithTriggerAttribute,
+            ManagementClient managementClient,
+            string? errorQueue = null,
+            string topicName = "bundle-1",
+            ILogger? logger = null)
+        {
+            var attribute = assemblyWithTriggerAttribute
+                .GetTypes()
+                .SelectMany(t => t.GetMethods())
+                .Where(m => m.GetCustomAttribute<FunctionNameAttribute>(false) != null)
+                .SelectMany(m => m.GetParameters())
+                .SelectMany(p => p.GetCustomAttributes<ServiceBusTriggerAttribute>(false))
+                .FirstOrDefault()
+                ?? throw new Exception("No endpoint was found");
+
+            var endpointQueueName = attribute.QueueName;
+
+            logger?.LogInformation("Queue Name: {queueName}", endpointQueueName);
+
+            errorQueue ??= $"{endpointQueueName}-error";
+
+            await CreateQueue(endpointQueueName, managementClient, logger);
+            await CreateQueue(errorQueue, managementClient, logger);
+
+            await CreateSubscription(topicName, managementClient, endpointQueueName, logger);
+        }
+
+        private static async Task CreateQueue(string endpointQueueName, ManagementClient managementClient, ILogger? logger)
+        {
+            if (await managementClient.QueueExistsAsync(endpointQueueName)) return;
+
+            logger?.LogInformation("Creating queue: `{queueName}`", endpointQueueName);
+            await managementClient.CreateQueueAsync(endpointQueueName);
+        }
+
+        private static async Task CreateSubscription(string topicName, ManagementClient managementClient, string endpointQueueName, ILogger? logger)
+        {
+            if (await managementClient.SubscriptionExistsAsync(topicName, endpointQueueName)) return;
+
+            logger?.LogInformation($"Creating subscription to: `{endpointQueueName}`", endpointQueueName);
+
+            var description = new SubscriptionDescription(topicName, endpointQueueName)
+            {
+                ForwardTo = endpointQueueName,
+                UserMetadata = $"Subscribed to {endpointQueueName}"
+            };
+
+            var ignoreAllEvents = new RuleDescription { Filter = new FalseFilter() };
+
+            await managementClient.CreateSubscriptionAsync(description, ignoreAllEvents);
+        }
+
+        public static IEnumerable<Assembly> GetAssemblies()
+        {
+            var list = new List<string>();
+            var stack = new Stack<Assembly>();
+
+            stack.Push(Assembly.GetEntryAssembly());
+
+            do
+            {
+                var asm = stack.Pop();
+
+                yield return asm;
+
+                foreach (var reference in asm.GetReferencedAssemblies())
+                    if (!list.Contains(reference.FullName))
+                    {
+                        stack.Push(Assembly.Load(reference));
+                        list.Add(reference.FullName);
+                    }
+            }
+            while (stack.Count > 0);
+        }
+    }
+}
