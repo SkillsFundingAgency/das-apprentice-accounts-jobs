1190c0401c866f1cab1a63c803cd84993c1f5bc5
Ben Arnold <ben@arnoldfamily.me.uk>
2021-12-09 13:58:48 +0000

    Extract HttpClient setup

---
+++ src/SFA.DAS.ApprenticeAccounts.Jobs/Infrastructure/AccountsApiClientConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using RestEase.HttpClientFactory;
+using SFA.DAS.ApprenticeAccounts.Jobs.Api;
+using System;
+
+namespace SFA.DAS.ApprenticeAccounts.Jobs.Infrastructure
+{
+    internal static class AccountsApiClientConfiguration
+    {
+        public static void AddAccountsApiClient(this IServiceCollection services)
+        {
+            services.AddTransient<Http.MessageHandlers.DefaultHeadersHandler>();
+            services.AddTransient<Http.MessageHandlers.LoggingMessageHandler>();
+            services.AddTransient<Http.MessageHandlers.ApimHeadersHandler>();
+
+            services.AddHttpClient("AccountsApi")
+                .ConfigureHttpClient((sp, client) =>
+                {
+                    var apiOptions = sp.GetRequiredService<ApiOptions>();
+                    client.BaseAddress = new Uri(apiOptions.ApiBaseUrl);
+                })
+                .UseWithRestEaseClient<IApprenticeAccountsApi>()
+                .AddHttpMessageHandler<Http.MessageHandlers.DefaultHeadersHandler>()
+                .AddHttpMessageHandler<Http.MessageHandlers.ApimHeadersHandler>()
+                .AddHttpMessageHandler<Http.MessageHandlers.LoggingMessageHandler>();
+        }
+    }
+}
--- src/SFA.DAS.ApprenticeAccounts.Jobs/Infrastructure/Startup.cs
+++ src/SFA.DAS.ApprenticeAccounts.Jobs/Infrastructure/Startup.cs
@@ -1,12 +1,9 @@
 ﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection;
 using NServiceBus;
-using RestEase.HttpClientFactory;
-using SFA.DAS.ApprenticeAccounts.Jobs.Api;
 using SFA.DAS.ApprenticeAccounts.Jobs.Infrastructure;
 using SFA.DAS.Http.Configuration;
 using SFA.DAS.NServiceBus.AzureFunctions.ServiceBus;
-using System;
 
 [assembly: FunctionsStartup(typeof(Startup))]
 [assembly: NServiceBusTriggerFunction(Startup.EndpointName)]
@@ -19,32 +16,19 @@ namespace SFA.DAS.ApprenticeAccounts.Jobs.Infrastructure
 
         public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder builder)
         {
-            //builder.ConfigureConfiguration();
+            builder.ConfigureConfiguration();
         }
 
         public override void Configure(IFunctionsHostBuilder builder)
         {
-            builder.Services.AddLogging();
+            builder.ConfigureLogging();
 
             builder.UseAzureFunctionNServiceBus(EndpointName);
 
             builder.Services.AddApplicationOptions();
             builder.Services.ConfigureFromOptions(f => f.ApprenticeAccountsApi);
             builder.Services.AddSingleton<IApimClientConfiguration>(x => x.GetRequiredService<ApiOptions>());
-            builder.Services.AddTransient<Http.MessageHandlers.DefaultHeadersHandler>();
-            builder.Services.AddTransient<Http.MessageHandlers.LoggingMessageHandler>();
-            builder.Services.AddTransient<Http.MessageHandlers.ApimHeadersHandler>();
-
-            builder.Services.AddHttpClient("example")
-                .ConfigureHttpClient((sp, client) =>
-                {
-                    var apiOptions = sp.GetRequiredService<ApiOptions>();
-                    client.BaseAddress = new Uri(apiOptions.ApiBaseUrl);
-                })
-                .UseWithRestEaseClient<IApprenticeAccountsApi>()
-                .AddHttpMessageHandler<Http.MessageHandlers.DefaultHeadersHandler>()
-                .AddHttpMessageHandler<Http.MessageHandlers.ApimHeadersHandler>()
-                .AddHttpMessageHandler<Http.MessageHandlers.LoggingMessageHandler>();
+            builder.Services.AddAccountsApiClient();
         }
     }
 }
