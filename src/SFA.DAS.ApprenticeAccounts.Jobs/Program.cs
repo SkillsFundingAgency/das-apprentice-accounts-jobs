b286c76ab6069e8545902dd8297470d5e78bcd51
alireid <ali.reid@live.co.uk>
2025-07-30 08:33:15 +0100

    Update Program.cs
    Experiment

--- src/SFA.DAS.ApprenticeAccounts.Jobs/Program.cs
+++ src/SFA.DAS.ApprenticeAccounts.Jobs/Program.cs
@@ -1,29 +1,29 @@
+using System.Diagnostics.CodeAnalysis;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using SFA.DAS.ApprenticeAccounts.Jobs.Infrastructure;
-
 [assembly: NServiceBusTriggerFunction(QueueNames.ApprenticeAccountsJobs)]
-var host = new HostBuilder()
 
-.ConfigureFunctionsWebApplication()
-.ConfigureAppConfiguration(
-    builder =>
+[ExcludeFromCodeCoverage]
+public static class Program
+{
+    public static async Task Main()
     {
-        builder.AddConfiguration();
-    })
-.ConfigureNServiceBus(QueueNames.ApprenticeAccountsJobs)
-
-.ConfigureServices((context, services) =>
- {
-     services
-
-        .AddApplicationInsightsTelemetryWorkerService()
-        .ConfigureFunctionsApplicationInsights()
-        .AddApplicationOptions()
-        .ConfigureFromOptions(f => f.ApprenticePortalOuterApi)
-        .AddServiceRegistrations();
- })
-
- .Build();
-await host.RunAsync();
+        var host = new HostBuilder()
+            .ConfigureFunctionsWebApplication()
+            .ConfigureAppConfiguration(builder => builder.AddConfiguration())
+            .ConfigureNServiceBus(QueueNames.ApprenticeAccountsJobs)
+            .ConfigureServices((context, services) =>
+            {
+                services
+                    .AddApplicationInsightsTelemetryWorkerService()
+                    .ConfigureFunctionsApplicationInsights()
+                    .AddApplicationOptions()
+                    .ConfigureFromOptions(f => f.ApprenticePortalOuterApi)
+                    .AddServiceRegistrations();
+            })
+            .Build();
+        await host.RunAsync();
+    }
+}
