4d3c54e6bb2905b166a3bd3e2ebc19387bc62188
NWake-CGI <nick.wake@cgi.com>
2025-09-16 08:58:56 +0100

    .NET8 Upgrade

--- src/SFA.DAS.ApprenticeAccounts.Jobs/EventHandlers/ApprenticeshipConfirmationEventHandlers/ApprenticeshipConfirmationConfirmedHandler.cs
+++ src/SFA.DAS.ApprenticeAccounts.Jobs/EventHandlers/ApprenticeshipConfirmationEventHandlers/ApprenticeshipConfirmationConfirmedHandler.cs
@@ -2,21 +2,24 @@
 using NServiceBus;
 using SFA.DAS.ApprenticeAccounts.Jobs.Api;
 using SFA.DAS.ApprenticeCommitments.Messages.Events;
+using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 
 namespace SFA.DAS.ApprenticeAccounts.Jobs.EventHandlers.ApprenticeshipConfirmationEventHandlers
 {
+    [ExcludeFromCodeCoverage]
     public class ApprenticeshipConfirmationConfirmedHandler : IHandleMessages<ApprenticeshipConfirmationConfirmedEvent>
     {
         private readonly ILogger<ApprenticeshipConfirmationConfirmedHandler> _logger;
         private readonly IOuterApiClient _outerApi;
-
+        
         public ApprenticeshipConfirmationConfirmedHandler(IOuterApiClient outerApi, ILogger<ApprenticeshipConfirmationConfirmedHandler> logger)
             => (_outerApi, _logger) = (outerApi, logger);
 
         public Task Handle(ApprenticeshipConfirmationConfirmedEvent message, IMessageHandlerContext context)
         {
-            _logger.LogInformation($"Received {nameof(ApprenticeshipConfirmationConfirmedEvent)} for apprentice {message.ApprenticeId}");
+            string logMessage = $"Received ApprenticeshipConfirmationConfirmedEvent for apprentice {message.ApprenticeId}";
+            _logger.LogInformation(logMessage);
 
             return _outerApi.SendApprenticeshipConfirmed(message.ApprenticeId, new ApprenticeshipConfirmedRequest {
                 CommitmentsApprenticeshipId = message.CommitmentsApprenticeshipId,
